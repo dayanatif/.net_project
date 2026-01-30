52f452ee9d001dcc301d437936c4941b6dbdfca5
Rao Muhammad Dayan Atif <raodayanatif@gmail.com>
2026-01-31 04:26:25 +0500

    Refactor Index.cshtml and _Layout.cshtml to enhance code readability by removing unnecessary invisible characters.

--- obj/Debug/net8.0/SoftwareSubscriptionApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoftwareSubscriptionApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftwareSubscriptionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df18754cbeac2c997da6a90a176ec0acb7f41d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftwareSubscriptionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftwareSubscriptionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
